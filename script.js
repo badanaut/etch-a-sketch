d98a9a5148b8789e331c4e35e402455b95b5694f
badanaut <49814350+badanaut@users.noreply.github.com>
2023-01-06 22:38:31 +0200

    Add color pick and clear grid
    Background-color on hover can be selected,
    clear grid function implemented - clears
    background of the grid squares
    
    [Ticket: 001]

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-hoverColor = "#5c5552"
-
+//get the clear button
+const clearBtn = document.getElementById('clear-btn');
 //get the grid div
 const container = document.querySelector('#container');
+//get the grid squares
+const squares = document.querySelectorAll('.grid-square');
+//get the color picker element
+const colorPicker = document.getElementById('pick-color');
+//default background-color on hover
+let hoverColor="#22223B";
+//default grid size 16x16
+let size=16;
+
+//clear grid when clear button is clicked
+clearBtn.addEventListener('click', function(){
+    //empty grid container
+    container.innerHTML = '';
+    //generate again grid content
+    createGrid(size);
+});
+
+
+
+colorPicker.addEventListener('input', function(){
+    hoverColor = colorPicker.value;
+});
 
 function changeColor(e) {
     e.target.style.backgroundColor = hoverColor;
 }
 
-function createDivs(size){
+function createGrid(){
     //generate grid elements
-    for(let i=0; i<size*size; i++){
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('grid-square')
-        newDiv.addEventListener('mouseover', changeColor);
+    for (let i = 0; i < size * size; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("grid-square");
+        newDiv.addEventListener("mouseover", changeColor);
+        newDiv.style.width = 100 / size + "%";
         container.appendChild(newDiv);
         
     }
 }
 
-createDivs(16);
+createGrid();
 
 
 
